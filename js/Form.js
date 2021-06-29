04e022484a79f1352c71b8f4b50523746b9a4b20
Pahul-07 <pahuldeep2021@gmail.com>
2021-06-29 17:06:31 +0530

    Class 39

--- js/Form.js
+++ js/Form.js
@@ -4,9 +4,28 @@ class Form{
 
     
     display(){
+        this.title=createElement("h2")
+        this.title.html("CAR RACING GAME");
+        this.title.position(130,200);
 
-       
+        this.input= createInput("Name");
+        this.input.position(160,250);
+
+        this.button= createButton("Play");
+        this.button.position(230,300);
+    
+        this.button.mousePressed( ()=>{
+            this.input.hide();
+            this.button.hide();
+
+            var name= this.input.value();
+
+            this.greetings= createElement("h1");
+            this.greetings.html("Hi "+ name);
+            this.greetings.position(150,300)
+        })     
     }
+
 
 
 }
--- sketch.js
+++ sketch.js
@@ -5,10 +5,12 @@ var form,game,player
 
 function setup(){
   database = firebase.database();
-  console.log(database);
+  console.log(database)
   createCanvas(500,500);
 
- 
+  game= new Game();
+  game.getState();
+  game.start();
   
 }
 
